19df163ce5463f482c5d01200354c03318d4de24
KazumaMurao <kazuma.murao@gmail.com>
2013-01-30 00:29:08 +0900

    using >> for ignore non-space seperator

--- c++/WORDCNT/WORDCNT.cpp
+++ c++/WORDCNT/WORDCNT.cpp
@@ -1,61 +1,43 @@
-#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-void solve(char* _str) {
-  int i = 0;
-  int begin = 0;
-  int end = 0;
-  int str_len = 0;
-  int cr_len = 0;
-  int cr_num = 1;
-  int num = 1;
-  int has_word = 0;
+void solve() {
+  int len;
+  int num = 0;
+  int pr_len = 0;
+  int cr_num = 0;
+  string line, s;
 
-  while (_str[i] != '\0') {
-    // printf("%c\n", _str[i]);
-    if (_str[i] == ' ') {
-      end = i;
-      str_len = end - begin;
-      if (str_len != 0) {
-        has_word = 1;
-        if (str_len == cr_len) {
-          cr_num++;
-        } else {
-          if (cr_num > num) {
-            num = cr_num;
-          }
-          cr_len = str_len;
-          cr_num = 1;
-        }
-      }
-      begin = end + 1;
+  getline(cin, line);
+  stringstream ss(line);
+
+  while (ss >> s) {
+    len = s.size();
+    if (len==pr_len) cr_num++;
+    else {
+      if (cr_num > num) num = cr_num;
+      cr_num = 1;
+      pr_len = len;
     }
-    i++;
-  } 
-
-  // adjusting
-  str_len = i - begin;
-  if (str_len != 0) {
-    has_word = 1;
-    if (str_len == cr_len) cr_num++;
-    if (cr_num > num) num = cr_num;
   }
-  if (has_word == 0) num = 0;
 
-  printf("%d\n", num);
+  if (cr_num > num) num = cr_num;
+  cout << num << endl;
+  
 }
 
 int main() {
+  string s;
   int iter;
-  char str[200000];
-  scanf("%d", &iter);
-  gets(str);
 
-  for (int i=0; i<iter; i++) {
-    gets(str);
-    solve(str);
+  cin >> iter;
+  getline(cin, s);
+
+  while (iter--) {
+    solve();
   }
-
   return 0;
 }
